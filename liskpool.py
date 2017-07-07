4a4a530b1466c10a1776c6b49ad06cb8c4ae2998
edvdkkb <edvdkkb@users.noreply.github.com>
2017-07-07 16:26:31 +0200

    Update liskpool.py

--- liskpool.py
+++ liskpool.py
@@ -34,7 +34,7 @@ def estimatePayouts (log):
 	d = requests.get (uri)
 	rew = d.json ()['rewards']
 	forged = (int (rew) / 100000000) * PERCENTAGE / 100
-	print ('To distribute: %f LSK' % forged)
+	print ('To distribute: %f SHIFT' % forged)
 	
 	if forged < 0.1:
 		return []
